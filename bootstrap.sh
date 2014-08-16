6c5f9f6f2093e3fcac0acc5cbc933924d16a91e9
Igor P. Leroy <ip.leroy@gmail.com>
2014-08-16 10:13:19 -0300

    Move vimfiles to vim-bootstrap

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-echo "Backuping vimfiles"
-for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone https://github.com/lerrua/vimfiles.git ~/.vim/
-ln -s ~/.vim/vimrc ~/.vimrc
-echo "Installing Vundle..."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "Installing Bundles"
-vim +BundleInstall +qall
-echo "Done! <3"
