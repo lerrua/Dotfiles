8dbf606b9d9fdb546027e813f1523ac9e610862a
Igor P. Leroy <ip.leroy@gmail.com>
2017-04-09 19:11:48 -0300

    Move vimfiles to vim folder

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -9,5 +9,5 @@ fancy_echo() {
 
 fancy_echo "Symlinking dotfiles"
 
-ln -s ~/Code/github-projects/Dotfiles/vimrc.local ~/.vimrc.bundles.local
-ln -s ~/Code/github-projects/Dotfiles/vimrc.local.bundles ~/.vimrc.bundles.local.bundles
+ln -s ~/Code/github-projects/Dotfiles/vim/vimrc.local ~/.vimrc.bundles.local
+ln -s ~/Code/github-projects/Dotfiles/vim/vimrc.local.bundles ~/.vimrc.bundles.local.bundles
