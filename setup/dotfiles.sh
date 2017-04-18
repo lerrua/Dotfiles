4f5b993dbe64508f8395a45716ccbe358d17ed71
Igor P. Leroy <ip.leroy@gmail.com>
2017-04-18 17:19:21 -0300

    Add tm command for screen choose

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -9,8 +9,10 @@ fancy_echo() {
 
 fancy_echo "Symlinking dotfiles"
 
-ln -s ~/Code/github-projects/Dotfiles/vim/vimrc.local ~/.vimrc.local
-ln -s ~/Code/github-projects/Dotfiles/vim/vimrc.local.bundles ~/.vimrc.local.bundles
+ln -s $DOTFILES/vim/vimrc.local ~/.vimrc.local
+ln -s $DOTFILES/vim/vimrc.local.bundles ~/.vimrc.local.bundles
 
-ln -s ~/Code/github-projects/Dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/Code/github-projects/Dotfiles/tmux/tmux.powerline ~/.tmux.powerline
+ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES/tmux/tmux.powerline ~/.tmux.powerline
+
+ln -s $DOTFILES/bin/tm  /usr/local/bin/tm
