e226799389f21fb35a9581a4e6f8c3af7316a2b0
Igor P. Leroy <ip.leroy@gmail.com>
2017-05-05 10:36:21 -0300

    Update tmuxconf to use Oh My Tmux as default

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -13,6 +13,6 @@ ln -s $DOTFILES/vim/vimrc.local ~/.vimrc.local
 ln -s $DOTFILES/vim/vimrc.local.bundles ~/.vimrc.local.bundles
 
 ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
-ln -s $DOTFILES/tmux/tmux.theme ~/.tmux.theme
+ln -s $DOTFILES/tmux/tmux.conf.local ~/.tmux.conf.local
 
 ln -s $DOTFILES/bin/tm  /usr/local/bin/tm
