3ebf51c6a05eee5be1e4c6a03d2c1563877b497f
Igor P. Leroy <ip.leroy@gmail.com>
2017-05-07 18:44:52 -0300

    Update setup.sh

---
+++ setup/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source ~/Code/github-projects/Dotfiles/setup/dotfiles.sh
+source ~/Code/github-projects/Dotfiles/setup/fonts.sh
+source ~/Code/github-projects/Dotfiles/setup/symlinks.sh
+source ~/Code/github-projects/Dotfiles/setup/bash.sh
