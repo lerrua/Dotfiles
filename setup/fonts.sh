dc102a58d8de42e7b7d908793d2227c666559a37
Igor P. Leroy <ip.leroy@gmail.com>
2017-05-14 11:06:06 -0300

    Change config to dev fonts and dev-icons

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font font-droidsansmono-nerd-font font-firacode-nerd-font
