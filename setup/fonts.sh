c7650966b8614aa7c89b9ce96677f0760e75a392
Igor P. Leroy <ip.leroy@gmail.com>
2017-07-11 15:48:36 -0300

    Add more font to default pack

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 brew tap caskroom/fonts
-brew cask install font-hack-nerd-font font-droidsansmono-nerd-font font-firacode-nerd-font
+brew cask install font-hack-nerd-font font-droidsansmono-nerd-font font-firacode-nerd-font font-fantasquesansmono-nerd-font
