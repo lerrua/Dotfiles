4428645f9d96fe24aa022ead3491324be3b0cc23
Igor P. Leroy <ip.leroy@gmail.com>
2017-07-24 11:19:10 -0300

    Fix manjaro installer to work with gnome

--- setup/manjaro/install.sh
+++ setup/manjaro/install.sh
@@ -1,6 +1,8 @@
 # install pacaur
 sudo pacman -Syu --noconfirm pacaur git vim
 
+sudo pacman -Syu --noconfirm base-devel
+
 export EDITOR="vim"
 
 # neovim dependencies
@@ -8,4 +10,4 @@ sudo easy_install pip
 sudo pip install neovim
 
 # install base packages
-pacaur -S --noconfirm --noedit ctags ncurses curl unzip vim neovim google-chrome firefox telegram-desktop-bin go dropbox slack docker docker-compose
+pacaur -S --noconfirm --noedit ctags ncurses curl unzip vim neovim google-chrome firefox telegram-desktop-bin go dropbox slack-desktop docker docker-compose
