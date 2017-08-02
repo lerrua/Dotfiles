d2b0f3f5a3a9371d7c2e87b688c7fa8447478a37
Igor P. Leroy <ip.leroy@gmail.com>
2017-08-02 17:56:30 -0300

    Add manjaro-linux base script

--- setup/manjaro/install.sh
+++ setup/manjaro/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# manjaro-gnome base config
 
 # install pacaur
 sudo pacman -Syu --noconfirm pacaur git vim
@@ -12,7 +13,10 @@ sudo easy_install pip
 sudo pip install neovim
 
 # install base packages
-pacaur -S --noconfirm --noedit ctags ncurses curl unzip vim python-neovim neovim google-chrome firefox telegram-desktop-bin go dropbox slack-desktop docker docker-compose tmux zsh htop fzf spotify steam
+pacaur -S --noconfirm --noedit ctags ncurses curl unzip vim python-neovim neovim google-chrome firefox go dropbox slack-desktop docker docker-compose tmux zsh htop fzf spotify steam-native steam xsel skypeforlinux-bin insomnia silver-searcher-git tree franz-bin
+
+# install useful gnome terminal themes
+wget https://raw.githubusercontent.com/romainl/apprentice-colorschemes/master/apprentice_gnome_terminal.sh
 
 echo "Now type \`sudo chsh -s $(which zsh)\` to zsh becomes default."
 echo "Logout and login to effective your changes."
