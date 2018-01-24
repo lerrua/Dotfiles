0266ae55097e5052054a42d7e01d3597f6983a1f
Igor P. Leroy <ip.leroy@gmail.com>
2018-01-24 17:13:11 -0200

    Remove tm bin symlink

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -25,4 +25,3 @@ ln -s $DOTFILES/neovim/local_bundles.vim $HOME/.config/nvim/local_bundles.vim
 # tmux
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/tmux.conf.local $HOME/.tmux.conf.local
-ln -s $DOTFILES/bin/tm  /usr/local/bin/tm
