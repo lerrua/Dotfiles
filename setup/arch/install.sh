241351c6544f31e7d2ea85a94f7fdd71fd1aec3f
Igor P. Leroy <ip.leroy@gmail.com>
2018-01-24 18:20:12 -0200

    Update arch installer

--- setup/arch/install.sh
+++ setup/arch/install.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 # Antergos base config
+
 # in case pure Arch read about how install pacaur: https://www.ostechnix.com/install-pacaur-arch-linux/
-
 # install pacaur
-sudo pacman -Syu --noconfirm pacaur git vim
+sudo pacman -Syu --noconfirm pacaur
 
+sudo pacman -Syu --noconfirm git vim
 sudo pacman -Syu --noconfirm base-devel
 
 export EDITOR="vim"
 
-# neovim dependencies
+# python and neovim dependencies
+sudo pacman -Syu --noconfirm python python-setuptools
 sudo easy_install pip
 sudo pip install neovim
 
 # install base packages
-pacaur -S --noconfirm --noedit ctags ncurses curl unzip python-neovim neovim go docker docker-compose tmux zsh htop fzf xsel silver-searcher-git tree exa dconf ranger
+pacaur -S --noconfirm --noedit ctags ncurses curl unzip neovim go docker docker-compose tmux zsh htop fzf xsel silver-searcher-git tree exa dconf ranger
 
 # install useful apps
 pacaur -S --noconfirm --noedit google-chrome dropbox slack-desktop spotify skypeforlinux-bin insomnia franz-bin wine ttf-vista-fonts wps-office steam-native-runtime
