0e80febe107d0248c4685b4f686e9a2bbd005857
Igor P. Leroy <ip.leroy@gmail.com>
2018-05-11 15:21:12 +0000

    Change initial config

--- setup/base.sh
+++ setup/base.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-# Antergos base config
+# antergos base config
 echo "Installing base-dev libs"
-sudo pacman -Syu --noconfirm git vim
 sudo pacman -Syu --noconfirm base-devel
+sudo pacman -Syu --noconfirm git vim
 
-# in case pure Arch read about how install pacaur: https://www.ostechnix.com/install-pacaur-arch-linux/
 # install pacaur
 echo "Using Pacaur as Arch package manager"
 sudo pacman -Syu --noconfirm pacaur
 
-
+# in case pure Arch read about how install pacaur: https://www.ostechnix.com/install-pacaur-arch-linux/
 # https://gist.github.com/Tadly/0e65d30f279a34c33e9b
 # git clone https://aur.archlinux.org/cower.git && cd cower && makepkg -i && cd -
 # git clone https://aur.archlinux.org/pacaur.git && cd pacaur && makepkg -i && cd -
