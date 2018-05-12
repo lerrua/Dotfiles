ca5771d3ace2c78bbf53448c065f290fd5cf29c1
Igor P. Leroy <ip.leroy@gmail.com>
2018-05-12 23:04:01 +0000

    Add nord theme on gnome-terminal

--- setup/terminal.sh
+++ setup/terminal.sh
@@ -11,6 +11,11 @@ wget -O xt https://git.io/v7eBS && chmod +x xt && ./xt && rm xt
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s /usr/bin/zsh
 
+# nord theme
+git clone git@github.com:arcticicestudio/nord-gnome-terminal.git $CODE/github/nord-gnome-terminal
+cd $CODE/github/nord-gnome-terminal/src/
+./nord.sh
+
 echo "----------- ZSH -----------"
 echo "Now type \`sudo chsh -s $(which zsh)\` to zsh becomes default."
 echo "Logout and login to effective your changes."
