3e7a88ba0e761d291d0cbfe5ea769bca18d51185
Igor P. Leroy <ip.leroy@gmail.com>
2018-09-25 15:06:34 -0300

    Remove gnome references to use base16

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -4,18 +4,12 @@ echo "Installing gnome-terminal"
 
 pacaur -S --noconfirm --noedit gnome-terminal
 
-# install gruvbox gnome terminal theme
-# http://mayccoll.github.io/Gogh/
-wget -O xt https://git.io/v7eBS && chmod +x xt && ./xt && rm xt
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s /usr/bin/zsh
-
-# nord theme
-git clone git@github.com:arcticicestudio/nord-gnome-terminal.git $CODE/github/nord-gnome-terminal
-cd $CODE/github/nord-gnome-terminal/src/
-./nord.sh
 
 echo "----------- ZSH -----------"
 echo "Now type \`sudo chsh -s $(which zsh)\` to zsh becomes default."
 echo "Logout and login to effective your changes."
+
+echo "Installing base16-shell"
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
