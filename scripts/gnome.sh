bc355cb8c9253a7c82f2f789718aa3790c23aae0
Igor P. Leroy <ip.leroy@gmail.com>
2018-12-31 12:12:09 -0200

    Add grub simple theme replacing antergos default

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 yay -S --noconfirm gtk-engine-murrine gtk-engines
 
+# gnome base theme
 [ -d ~/.themes ] && echo "Directory 'themes' already exists" || mkdir -p ~/.themes
 cd ~/.themes && git clone https://github.com/paullinuxthemer/Telinkrin-GTK.git
 ln -s ~/.themes/Telinkrin-GTK/Telinkrin-Buttons-Left=\>3.30 ~/.themes/Telinkrin-Buttons-Right=\>3.30
+
+# grub base theme
+wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash
