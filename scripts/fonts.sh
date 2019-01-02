ffd1442529ec788c8c66a781b05e1a32e1efadaa
Igor P. Leroy <ip.leroy@gmail.com>
2019-01-02 16:49:27 -0200

    Fix fonts url

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 echo "Installing fonts"
+_installMany noto-fonts ttf-ubuntu-font-family ttf-droid
 
-yay -S noto-fonts --noconfirm --needed
-yay -S ttf-ubuntu-font-family --noconfirm --needed
-yay -S ttf-droid --noconfirm --noconfirm --needed
-
+echo "export custom xterm font"
 tic xterm-256color-italic
 
 echo "Get nerdfonts at https://github.com/ryanoasis/nerd-fonts"
+[ -d ~/.local/share/fonts ] && echo "Directory '.local/share/fonts' exists" || mkdir -p ~/.local/share/fonts
 
-# mkdir -p ~/.local/share/fonts
-# cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf
 
-# cd ~/.local/share/fonts && curl -fLo "Knack Regular Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Knack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+cd ~/.local/share/fonts && curl -fLo "Knack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
 fc-cache -v
