cb2f49c942ffebb88e0621a832560ec3788b3fd9
Igor P. Leroy <ip.leroy@gmail.com>
2019-01-08 11:36:32 -0200

    Add some Go apps

--- scripts/devel.sh
+++ scripts/devel.sh
@@ -15,8 +15,10 @@ _installMany ctags ncurses curl unzip neovim go docker docker-compose tmux
 _installMany zsh fzf xsel silver-searcher-git tree exa dconf ranger mosh
 _installMany tig bat prettyping ncdu wget
 
-echo "Setting Rust environment"
+echo "Installing Go tools"
+go get -v github.com/sosedoff/pgweb 
 
+echo "Setting Rust environment"
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 rustup install nightly
